7ae88118615ff4dc3e0594f7e026fc725195ff47
Pedro Henrique Crua Lima SIlva <167873867+phsilvadev@users.noreply.github.com>
2026-03-18 13:06:14 -0300

    tentando mudar dominio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,6 +45,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
+        <link rel="canonical" href="https://www.acomoreira.com.br" />
         <meta name="theme-color" content="#dc2626" />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </head>
